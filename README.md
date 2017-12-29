e40b4bc6c4c5bf79d1b9da9c681e8cd9436974c7
jflewis <lewisjnerd@gmail.com>
2017-12-29 14:17:47 -0500

    Log hostname instead of IP.
    Performs a reverse lookup on the IP to try to resolve the hostname.
    Added a README.md
    Pushing to github

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rusty Data Hound
+
+Data hound will listen and log all traffic on 80 and 443 (excluding SYN, ACK, and FIN packets). Will log the hostname and the number of requests it has sent to your computer.
+
+## History
+I wanted a way to monitor all the web traffic my computer was reciving, specially to view all the garbage requests for ads and spam. 
+This was strictly for fun to teach myself Rust and pcap.
+
+
+*I can safely guarantee this will not work well ;) but it was fun to build.*
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 extern crate pcap;
+extern crate termion;
+extern crate dns_lookup;
+
+use std::collections::HashMap;
+use dns_lookup::lookup_addr;
 
 static ETHERNET_HEADER_SIZE: usize = 14;
 #[derive(Debug)] // not necessary
@@ -18,30 +23,43 @@ struct IpHeader{
 
 impl IpHeader {
    fn source_ip(&self) -> String {
-       format!( "{}.{}.{}.{}", self.saddr[0],self.saddr[1],self.saddr[2],self.saddr[3])
+       format!( "{}.{}.{}.{}", self.saddr[0], self.saddr[1], self.saddr[2], self.saddr[3])
    }
 
    fn destination_ip(&self) -> String {
-       format!( "{}.{}.{}.{}", self.daddr[0],self.daddr[1],self.daddr[2],self.daddr[3])
+       format!( "{}.{}.{}.{}", self.daddr[0], self.daddr[1], self.daddr[2], self.daddr[3])
    }
 }
 
 fn main() {
-    let mut cap = pcap::Capture::from_device("en5").unwrap().open().unwrap();
-    // To capture all IPv4 HTTP packets to and from port 443, i.e. print only packets that contain data, not, for example, SYN and FIN packets and ACK-only packets. (IPv6 is left as an exercise for the reader.)
+    let mut cap = pcap::Capture::from_device("en0").unwrap().open().unwrap();
+    // To capture all IPv4 HTTP packets to and from port 443, i.e. print only packets that contain data, not, for example, SYN and FIN packets and ACK-only packets.
     cap.filter("tcp port 443 or 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)").unwrap();
 
+    let mut ip_count: HashMap<String, u16> = HashMap::new();
+    println!("Starting to listen....", );
     while let Ok(packet) = cap.next() {
+        println!("    Address    |    ip      ");
+        println!("----------------------------");
         let eth = &packet.data[ETHERNET_HEADER_SIZE..];
 
-        // TODO: Look into using byteorder crate or using transmute. This works but it's funky
         let ip_header: IpHeader = unsafe{
            std::ptr::read(eth.as_ptr() as *const _) 
         };
 
-        println!("ip version: {}", (ip_header.ver_ihl & 0b11110000) >> 4 );
-        print!("source IP: {} -> ", ip_header.source_ip() );
-        println!("destination IP: {}", ip_header.destination_ip() )
+        if ip_header.source_ip() != "192.168.1.156" {
+            // perform reverse lookup of IP address
+            let ip: std::net::IpAddr = ip_header.source_ip().parse().unwrap();
+            let hostname = lookup_addr(&ip).unwrap();
+            // Ensures a value is in the entry by inserting the default if empty, and returns a mutable reference to the value in the entry.
+            let counter = ip_count.entry(hostname).or_insert(1);
+            *counter += 1;
+        }
+        
+        for (address, count) in &ip_count {
+            println!("address: {i}, count: {c}", i=address, c=count);
+        }
 
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
     }
 }
